a7511a800e21965d8055bd67f72cfb6d149d033d
dmitrygrig <dmitrygrig@gmail.com>
2015-06-28 12:59:10 +0200

    add install batch (bash for UNIX) script for installing jBayes-r

---
+++ jbayes-r/install.bat
@@ -0,0 +1,4 @@
+call mvn install:install-file -Dfile=libs\rJava\JRIEngine.jar -DgroupId=net.rforge -DartifactId=JRIEngine -Dversion=0.9.7 -Dpackaging=jar
+call mvn install:install-file -Dfile=libs\rJava\REngine.jar -DgroupId=net.rforge -DartifactId=REngine -Dversion=0.9.7 -Dpackaging=jar
+call mvn install:install-file -Dfile=libs\rJava\JRI.jar -DgroupId=net.rforge -DartifactId=JRI -Dversion=0.9.7 -Dpackaging=jar
+call mvn install
---
+++ jbayes-r/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mvn install:install-file -Dfile=libs\rJava\JRIEngine.jar -DgroupId=net.rforge -DartifactId=JRIEngine -Dversion=0.9.7 -Dpackaging=jar
+mvn install:install-file -Dfile=libs\rJava\REngine.jar -DgroupId=net.rforge -DartifactId=REngine -Dversion=0.9.7 -Dpackaging=jar
+mvn install:install-file -Dfile=libs\rJava\JRI.jar -DgroupId=net.rforge -DartifactId=JRI -Dversion=0.9.7 -Dpackaging=jar
+mvn install
